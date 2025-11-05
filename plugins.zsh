d7cf49a8b74a44a77c0d75d43b027c6c3f586b47
David Liu <davidas99017@gmail.com>
2025-11-05 10:24:31 -0800

    updated plugins

--- plugins.zsh
+++ plugins.zsh
@@ -10,8 +10,13 @@ if ! zgenom saved; then
   zgenom oh-my-zsh plugins/git
   zgenom oh-my-zsh plugins/sudo
   zgenom oh-my-zsh plugins/starship
+  zgenom oh-my-zsh plugins/colored-man-pages
+
   zgenom oh-my-zsh plugins/command-not-found
   zgenom oh-my-zsh plugins/cp
+  zgenom oh-my-zsh plugins/fzf
+  zgenom oh-my-zsh plugins/gh 
+  zgenom oh-my-zsh plugins/thefuck
   zgenom oh-my-zsh plugins/docker
   zgenom oh-my-zsh plugins/docker-compose
   zgenom oh-my-zsh plugins/flutter
